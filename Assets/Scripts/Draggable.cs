376a5d225646503fb81279d088e75773819ffd34
MARSHAL327 <sanya.shvedenko@mail.ru>
2021-01-02 00:01:35 +0300

    Добавление всех файлов в репозиторий

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : EventTrigger
+{
+    public bool dragging;
+    private Vector2 dragOffset;
+
+    public void Update()
+    {
+        if (dragging)
+        {
+            transform.position = (Vector2)Input.mousePosition + dragOffset;
+        }
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+
+        dragging = true;
+        dragOffset = (Vector2)transform.position - (Vector2)Input.mousePosition;
+    }
+
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        dragging = false;
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Room : MonoBehaviour
+{
+    private Camera Cam;
+    private string markName;
+    private Button room;
+
+    void Start()
+    {
+        room = GetComponent<Button>();
+    }
+
+    void Update()
+    {
+    }
+
+    public void CreateMark(string markName) {
+
+    }
+}
